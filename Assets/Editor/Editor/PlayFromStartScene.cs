99af49daf5b80f81268b7417e0971b6b7558305b
Seyed Morteza Kamali <smkplus3d@gmail.com>
2019-11-10 20:19:26 +0330

    FirstCommit

---
+++ Assets/Editor/Editor/PlayFromStartScene.cs
@@ -0,0 +1,86 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+
+namespace UnityToolbarExtender
+{
+    static class ToolbarStyles
+    {
+        public static readonly GUIStyle commandButtonStyle;
+
+        static ToolbarStyles()
+        {
+            commandButtonStyle = new GUIStyle("Command")
+            {
+                fontSize = 16,
+                alignment = TextAnchor.MiddleCenter,
+                imagePosition = ImagePosition.ImageAbove,
+                fontStyle = FontStyle.Bold
+            };
+        }
+    }
+    
+
+    [InitializeOnLoad]
+    public class SceneSwitchLeftButton
+    {
+        private static bool _deleteKeys;
+
+        static SceneSwitchLeftButton()
+        {
+            ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+            EditorApplication.playModeStateChanged += LogPlayModeState;
+        }
+
+        static void OnToolbarGUI()
+        {
+            GUILayout.FlexibleSpace();
+            
+            if (_deleteKeys)
+            {
+                if (GUILayout.Button(EditorGUIUtility.IconContent("SavePassive"), ToolbarStyles.commandButtonStyle)) _deleteKeys = false;
+            }
+            else
+            {
+                if (GUILayout.Button(EditorGUIUtility.IconContent("SaveActive"), ToolbarStyles.commandButtonStyle)) _deleteKeys = true;
+            }
+ 
+
+            
+            if (GUILayout.Button(EditorGUIUtility.IconContent("SaveFromPlay"), ToolbarStyles.commandButtonStyle))
+            {
+                PlayerPrefs.DeleteAll();
+            }
+
+            if (_deleteKeys)
+            {
+                PlayerPrefs.DeleteAll();
+            }
+
+            if (GUILayout.Button(EditorGUIUtility.IconContent("LookDevSingle1@2x"), ToolbarStyles.commandButtonStyle))
+            {
+                if (!EditorApplication.isPlaying)
+                {
+                    EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+                    EditorPrefs.SetInt("LastActiveScene", EditorSceneManager.GetActiveScene().buildIndex);
+                    EditorSceneManager.OpenScene(SceneUtility.GetScenePathByBuildIndex(0));
+                }
+
+                EditorApplication.isPlaying = !EditorApplication.isPlaying;
+            }
+            
+
+        }
+
+        private static void LogPlayModeState(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.EnteredEditMode && EditorPrefs.HasKey("LastActiveScene"))
+            {
+                EditorSceneManager.OpenScene(
+                    SceneUtility.GetScenePathByBuildIndex(EditorPrefs.GetInt("LastActiveScene")));
+                EditorPrefs.DeleteKey("LastActiveScene");
+            }
+        }
+    }
+}
---
+++ Assets/Editor/Editor/RestartPlayMode.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityToolbarExtender
+{
+    
+    [InitializeOnLoad]
+    public static class RestartPlayMode
+    {
+
+        static RestartPlayMode()
+        {
+            ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
+        }
+        static void OnToolbarGUI()
+        {
+
+            
+            EditorGUIUtility.SetIconSize(new Vector2(17,17));
+            if (GUILayout.Button(EditorGUIUtility.IconContent("LookDevResetEnv@2x"), ToolbarStyles.commandButtonStyle))
+            {
+                if (EditorApplication.isPlaying)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            }
+
+            
+            Time.timeScale = EditorGUILayout.Slider("", Time.timeScale, 1, 20,GUILayout.Width(150));
+
+        }
+    }
+}
